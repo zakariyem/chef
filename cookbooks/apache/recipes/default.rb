2c2b5639feead272918422a4b7580d0df491fcb8
zakariye88 <zakariye88@yahoo.com>
2017-12-07 19:58:33 +0000

    Added basic apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+#
+package 'apache2' do
+        package_name 'httpd'
+        action :install
+end
+
+service 'apache2'  do
+        service_name 'httpd'
+        action [:start, :enable]
+end
+
+
